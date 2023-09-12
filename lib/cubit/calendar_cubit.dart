2b87edcef2cd3a553a786e37053833a28e047d5a
mahmoud <7ive.123@gmail.com>
2023-09-13 02:22:41 +0300

    initial commit

--- lib/cubit/calendar_cubit.dart
+++ lib/cubit/calendar_cubit.dart
@@ -55,7 +55,7 @@ class CalendarCubit extends Cubit<CalendarStates> {
     emit(ChangeNotificationState());
   }
 
-//sqflite
+//sqflite local database
   Database? database;
   void openDataBase() {
     emit(LoadingOpenDatabaseState());
