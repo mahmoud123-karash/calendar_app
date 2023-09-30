4b6a1f124bb6758d8351773c4ce647b8df5db1dc
mahmoud <7ive.123@gmail.com>
2023-09-30 22:01:08 +0300

    edit one commit

--- lib/modules/navigation_bar_screen/navigation_bar_screen.dart
+++ lib/modules/navigation_bar_screen/navigation_bar_screen.dart
@@ -11,36 +11,37 @@ class NavigationBarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CalendarCubit, CalendarStates>(
-        builder: (context, state) {
-      bool isdark = CalendarCubit.get(context).isdark;
-      return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: !isdark ? Colors.white : blackColor,
-          type: BottomNavigationBarType.fixed,
-          unselectedItemColor: isdark ? Colors.white60 : Colors.black,
-          fixedColor: myColor,
-          currentIndex: CalendarCubit.get(context).currentindex,
-          onTap: (value) {
-            CalendarCubit.get(context).getindex(value);
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.calendar_view_day_outlined),
-              label: S.of(context).title,
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.calendar_today_outlined),
-              label: S.of(context).myevent,
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.settings),
-              label: S.of(context).settings,
-            ),
-          ],
-        ),
-        body: CalendarCubit.get(context)
-            .screens[CalendarCubit.get(context).currentindex],
-      );
-    });
+      builder: (context, state) {
+        bool isdark = CalendarCubit.get(context).isdark;
+        return Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: !isdark ? Colors.white : blackColor,
+            type: BottomNavigationBarType.fixed,
+            unselectedItemColor: isdark ? Colors.white60 : Colors.black,
+            fixedColor: myColor,
+            currentIndex: CalendarCubit.get(context).currentindex,
+            onTap: (value) {
+              CalendarCubit.get(context).getindex(value);
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.calendar_view_day_outlined),
+                label: S.of(context).title,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.calendar_today_outlined),
+                label: S.of(context).myevent,
+              ),
+              BottomNavigationBarItem(
+                icon: const Icon(Icons.settings),
+                label: S.of(context).settings,
+              ),
+            ],
+          ),
+          body: CalendarCubit.get(context)
+              .screens[CalendarCubit.get(context).currentindex],
+        );
+      },
+    );
   }
 }
